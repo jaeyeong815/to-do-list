790c0278a136738052377d896abaa33b432430ea
jaeyeong815 <jae0ee@naver.com>
2022-10-05 22:28:35 +0900

    useState 코드 작성 중

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,21 +1,64 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 function Login() {
+  const [loginData, setLoginData] = useState({});
+  const [disabled, setDisabled] = useState(true);
+
+  function onChangeHandle(e) {
+    const { value, name } = e.target;
+    setLoginData({
+      ...loginData,
+      [name]: value,
+    });
+  }
+
+  function validityCheck() {
+    if (loginData.email.includes('@') && loginData.password.length >= 8) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }
+
+  function handleSubmit() {}
+
   return (
     <Wrapper>
       <LoginForm>
         <div className="email">
           <H4 className="email">이메일</H4>
-          <Input type="email" />
+          <Input
+            type="email"
+            name="email"
+            placeholder="이메일을 입력해주세요."
+            value={loginData.email || ''}
+            onChange={onChangeHandle}
+          />
         </div>
         <div className="password">
           <H4 className="password">비밀번호</H4>
-          <Input type="password" />
+          <Input
+            type="password"
+            name="password"
+            placeholder="비밀번호를 입력해주세요."
+            value={loginData.password || ''}
+            onChange={onChangeHandle}
+          />
         </div>
-        <button type="submit">로그인</button>
+        <Button
+          type="submit"
+          disabled={
+            loginData.email.includes('@') && loginData.password.length >= 8
+              ? false
+              : true
+          }
+        >
+          로그인
+        </Button>
       </LoginForm>
-      <Link to="signup">회원가입</Link>
+      <StyledLink to="signup">회원가입</StyledLink>
     </Wrapper>
   );
 }
@@ -42,6 +85,22 @@ const H4 = styled.h4`
 
 const Input = styled.input`
   margin-bottom: 10px;
+  width: 200px;
+  height: 25px;
+`;
+
+const Button = styled.button`
+  padding: 10px 25px;
+  margin-bottom: 10px;
+
+  border: none;
+  border-radius: 5px;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  font-size: small;
+  color: grey;
 `;
 
 export default Login;
