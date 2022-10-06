3ec9f43518fd7d52d0bf271e324796c91348842a
jaeyeong815 <jae0ee@naver.com>
2022-10-06 17:33:28 +0900

    feat: 회원가입 기능

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
 import { Wrapper, LoginForm, H4, Input, Button, StyledLink } from './Style';
+import Axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function SignUp() {
   const [userData, setUserData] = useState({});
+  const [isEmail, setIsEmail] = useState(false);
+  const [isPassword, setIsPassword] = useState(false);
+  const navigate = useNavigate();
 
   function onChangeHandle(e) {
     const { value, name } = e.target;
@@ -10,18 +15,46 @@ function SignUp() {
       ...userData,
       [name]: value,
     });
+
+    if (userData?.email?.includes('@')) {
+      setIsEmail(true);
+    } else {
+      setIsEmail(false);
+    }
+
+    if (userData?.password?.length > 6) {
+      setIsPassword(true);
+    } else {
+      setIsPassword(false);
+    }
+  }
+  async function onSubmitHandle(e) {
+    e.preventDefault();
+    await Axios.post(
+      `https://pre-onboarding-selection-task.shop/auth/signup`,
+      {
+        email: userData.email,
+        password: userData.password,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    navigate('/');
   }
   return (
     <>
       <Wrapper>
-        <LoginForm>
+        <LoginForm onSubmit={onSubmitHandle}>
           <div className="email">
             <H4 className="email">이메일</H4>
             <Input
               type="email"
               name="email"
               placeholder="이메일을 입력해주세요."
-              value={userData?.email}
+              value={userData?.email || ''}
               onChange={onChangeHandle}
             />
           </div>
@@ -31,11 +64,13 @@ function SignUp() {
               type="password"
               name="password"
               placeholder="비밀번호를 입력해주세요."
-              value={userData?.password}
+              value={userData?.password || ''}
               onChange={onChangeHandle}
             />
           </div>
-          <Button type="submit">회원가입</Button>
+          <Button type="submit" disabled={!(isEmail && isPassword)}>
+            회원가입
+          </Button>
         </LoginForm>
         <StyledLink to="/">로그인</StyledLink>
       </Wrapper>
