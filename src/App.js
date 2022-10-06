90b6495e562c1064ba9481ae17ec48d7c80fa516
jaeyeong815 <jae0ee@naver.com>
2022-10-06 17:39:29 +0900

    feat: todo 화면 초기 세팅

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
+import Todo from './components/Todo';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/todo" element={<Todo />} />
       </Routes>
     </Router>
   );
---
+++ src/components/Todo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Input, Button } from './Style';
+
+function Todo() {
+  return (
+    <>
+      <h1>투두 리스트</h1>
+      <Input />
+      <Button>추가</Button>
+    </>
+  );
+}
+
+export default Todo;
