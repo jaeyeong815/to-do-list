5d3abcfef432bf168639674298f704fffe6a3924
jaeyeong815 <jae0ee@naver.com>
2022-10-08 10:53:10 +0900

    fix: 투두리스트 수정 시 전체 인풋이 열리는 오류 수정

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { Input, Button, H4 } from './Style';
 import Axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-
 import { isLogin } from '../util/isLogin';
 
 function Todo() {
   const [todoData, setTodoData] = useState([]);
   const [text, setText] = useState('');
   const [isUpdate, setIsUpdate] = useState(false);
+  const [updateId, setUpdateId] = useState('');
   const [updateText, setUpdateText] = useState(text);
   const [isCompleted, setIsCompleted] = useState(false);
   const token = localStorage.getItem('token');
@@ -63,10 +63,9 @@ function Todo() {
     );
   }
 
-  async function updateHandle(id) {
-    //fix 수정버튼 클릭 시 모든 할일이 수정모드가 됨
+  async function updateHandle() {
     await Axios.put(
-      `https://pre-onboarding-selection-task.shop/todos/${id}`,
+      `https://pre-onboarding-selection-task.shop/todos/${updateId}`,
       { todo: updateText, isCompleted },
       {
         headers: {
@@ -76,10 +75,10 @@ function Todo() {
       }
     ).then((res) => setIsUpdate(false));
   }
+
   //todo 투두 완료여부 디자인 필요
   //todo ul이 점으로 되어있는데 수정할 수 있으면 하기 (디자인)
   //todo 수정,삭제,취소 버튼 디자인 필요
-
   return (
     <>
       <h1>투두 리스트</h1>
@@ -92,27 +91,27 @@ function Todo() {
       <Button onClick={createTodo}>추가</Button>
       <H4>해야할 일</H4>
       <ul>
-        {todoData &&
-          todoData.map((todo) => {
-            return isUpdate ? (
-              <li key={todo.id}>
-                <input type="checkbox" onChange={checkedCompleted} />
-                <Input
-                  value={updateText}
-                  onChange={(e) => setUpdateText(e.target.value)}
-                />
-                <button onClick={() => updateHandle(todo.id)}>제출</button>
-                <button
-                  onClick={() => {
-                    setIsUpdate(false);
-                    setText('');
-                    setIsCompleted(todo.isCompleted);
-                  }}
-                >
-                  취소
-                </button>
-              </li>
-            ) : (
+        {isUpdate ? (
+          <li key={updateId}>
+            <input type="checkbox" onChange={checkedCompleted} />
+            <Input
+              value={updateText}
+              onChange={(e) => setUpdateText(e.target.value)}
+            />
+            <button onClick={() => updateHandle()}>제출</button>
+            <button
+              onClick={() => {
+                setIsUpdate(false);
+                setText('');
+                setIsCompleted(false);
+              }}
+            >
+              취소
+            </button>
+          </li>
+        ) : (
+          todoData?.map((todo) => {
+            return (
               <li key={todo.id}>
                 {isCompleted ? <H4>완료!</H4> : <H4>아직</H4>}
                 {todo.todo}
@@ -120,6 +119,8 @@ function Todo() {
                   onClick={() => {
                     setIsUpdate(true);
                     setUpdateText(todo.todo);
+                    setUpdateId(todo.id);
+                    // updateHandle(todo.id);
                   }}
                 >
                   수정
@@ -127,10 +128,17 @@ function Todo() {
                 <button onClick={() => deleteTodo(todo.id)}>삭제</button>
               </li>
             );
-          })}
+          })
+        )}
       </ul>
     </>
   );
 }
 
 export default Todo;
+
+// 투두데이터 있으면 해야할일 목록이 나온다
+// 수정버튼을 누르면 isUpdate가 true로 변경된다
+// 수정창이 뜬다 -> 수정 할 인풋만 활성화
+// 수정창을 끄면 false가 된다
+// 수정을 누르면 handle함수가 실행된다
