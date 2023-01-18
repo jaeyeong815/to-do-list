861089ea75e9cef90a5f6997f9a86ef11e75ce53
jaeyeong815 <jae0ee@naver.com>
2023-01-18 14:48:38 +0900

    rename: #1 utils로 폴더명 변경

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Axios from 'axios';
-import { isLogin } from '../../util/isLogin';
+import { isLogin } from '../../utils/isLogin';
 import { Wrapper, TodoInput, Button, TodoBtn, Span, LiWrapper } from '../../styles/Style';
 
 function Todo() {
