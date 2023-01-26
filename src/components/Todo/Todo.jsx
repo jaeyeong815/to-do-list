09f4c7852b7a3180344ede6ab6b344f8d0d37cc4
jaeyeong815 <jae0ee@naver.com>
2023-01-26 23:14:10 +0900

    feat: #7 Logout 컴포넌트 및 기능 구현

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useContext } from 'react';
 import AddTodo from './AddTodo';
 import TodoList from './TodoList';
 import EditTodo from './EditTodo';
+import Logout from '../Auth/Logout';
 import { TodoContext } from '../../context/TodoContext';
 import { Wrapper, Ul } from '../../styles/Style';
 
@@ -17,6 +18,7 @@ function Todo() {
       <h1>투두 리스트</h1>
       <AddTodo />
       <Ul>{todoCtx.isUpdating ? <EditTodo /> : <TodoList />}</Ul>
+      <Logout />
     </Wrapper>
   );
 }
